65daf868916728f6b3b78fea056fdb0d4fb6b7e6
Shantanu Raj <s@sraj.me>
2021-06-20 15:16:55 +0200

    🕐️ Show duration of running entry (#3)
    * 🕐️ Show duration of running entry
    
    - Run cargo fmt on modified files
    - Show fallback description if TE description is empty
    
    * ⌨️  Move TE summary to Display trait implementation

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ mod arguments;
 mod credentials;
 mod models;
 use api::ApiClient;
-use arguments::CommandLineArguments;
 use arguments::Command;
 use arguments::Command::Auth;
 use arguments::Command::Current;
 use arguments::Command::Running;
-use arguments::Command::Stop;
 use arguments::Command::Start;
+use arguments::Command::Stop;
+use arguments::CommandLineArguments;
 use credentials::Credentials;
 use models::ResultWithDefaultError;
 use structopt::StructOpt;
@@ -26,9 +26,12 @@ pub async fn execute_subcommand(command: Option<Command>) -> ResultWithDefaultEr
         Some(subcommand) => match subcommand {
             Current | Running => display_running_time_entry().await?,
             Stop => stop_running_time_entry().await?,
-            Start { description: _, project: _ } => (),
-            Auth { api_token } => authenticate(api_token).await?
-        }
+            Start {
+                description: _,
+                project: _,
+            } => (),
+            Auth { api_token } => authenticate(api_token).await?,
+        },
     }
 
     Ok(())
@@ -40,17 +43,22 @@ fn ensure_authentication() -> ResultWithDefaultError<ApiClient> {
         Err(err) => {
             println!("Please set your API token first by calling toggl auth <API_TOKEN>.");
             println!("You can find your API token at https://track.toggl.com/profile.");
-            return Err(err)
+            return Err(err);
         }
-    }
+    };
 }
 
 async fn authenticate(api_token: String) -> ResultWithDefaultError<()> {
-    let credentials = Credentials { api_token: api_token };
+    let credentials = Credentials {
+        api_token: api_token,
+    };
     let api_client = ApiClient::from_credentials(credentials)?;
     let user = api_client.get_user().await?;
     let _credentials = Credentials::persist(user.api_token)?;
-    println!("Successfully authenticated for user with email {}", user.email);
+    println!(
+        "Successfully authenticated for user with email {}",
+        user.email
+    );
 
     return Ok(());
 }
@@ -59,7 +67,7 @@ async fn display_running_time_entry() -> ResultWithDefaultError<()> {
     let api_client = ensure_authentication()?;
     match api_client.get_running_time_entry().await? {
         None => println!("No time entry is running at the moment"),
-        Some(running_time_entry) => println!("{}", running_time_entry.description)
+        Some(running_time_entry) => println!("{}", running_time_entry),
     }
 
     return Ok(());
