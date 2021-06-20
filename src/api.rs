568d64de09a431706042ad18442df426e430d628
William Barbosa <heytherewill@gmail.com>
2021-06-20 16:54:16 +0200

    🛰️ Move implementation of API to a trait (#18)
    * 🐌 Add the async trait crate
    
    * 📡 Move API methods to the ApiClient trait
    
    * 🏗️ Apply façade changes to main.rs

--- src/api.rs
+++ src/api.rs
@@ -1,21 +1,74 @@
 use crate::credentials;
 use crate::models;
+use async_trait::async_trait;
 use chrono::Utc;
 use models::{ResultWithDefaultError, TimeEntry, User};
 use reqwest::{header, Client};
 use serde::{de, Serialize};
 
-pub struct ApiClient {
+const CLIENT_NAME: &'static str = "github.com/heytherewill/toggl-cli";
+
+#[async_trait]
+pub trait ApiClient {
+    async fn get_user(&self) -> ResultWithDefaultError<User>;
+    async fn get_running_time_entry(&self) -> ResultWithDefaultError<Option<TimeEntry>>;
+    async fn get_time_entries(&self) -> ResultWithDefaultError<Vec<TimeEntry>>;
+    async fn start_time_entry(&self, time_entry: TimeEntry) -> ResultWithDefaultError<TimeEntry>;
+    async fn stop_time_entry(&self, time_entry: TimeEntry) -> ResultWithDefaultError<TimeEntry>;
+}
+
+pub struct V9ApiClient {
     http_client: Client,
     base_url: String,
 }
 
-const CLIENT_NAME: &'static str = "github.com/heytherewill/toggl-cli";
+#[async_trait]
+impl ApiClient for V9ApiClient {
+    async fn get_user(&self) -> ResultWithDefaultError<User> {
+        let url = format!("{}/me", self.base_url);
+        return self.get::<User>(url).await;
+    }
 
-impl ApiClient {
+    async fn get_running_time_entry(&self) -> ResultWithDefaultError<Option<TimeEntry>> {
+        let url = format!("{}/me/time_entries/current", self.base_url);
+        return self.get::<Option<TimeEntry>>(url).await;
+    }
+
+    async fn get_time_entries(&self) -> ResultWithDefaultError<Vec<TimeEntry>> {
+        let url = format!("{}/me/time_entries", self.base_url);
+        return self.get::<Vec<TimeEntry>>(url).await;
+    }
+
+    async fn start_time_entry(&self, time_entry: TimeEntry) -> ResultWithDefaultError<TimeEntry> {
+        let url = format!("{}/time_entries", self.base_url);
+        let time_entry_to_create = TimeEntry {
+            created_with: Some(CLIENT_NAME.to_string()),
+            ..time_entry
+        };
+        return self
+            .post::<TimeEntry, TimeEntry>(url, &time_entry_to_create)
+            .await;
+    }
+
+    async fn stop_time_entry(&self, time_entry: TimeEntry) -> ResultWithDefaultError<TimeEntry> {
+        let stop_time = Utc::now();
+        let stopped_time_entry = TimeEntry {
+            stop: Some(stop_time),
+            duration: (stop_time - time_entry.start).num_seconds(),
+            ..time_entry
+        };
+
+        let url = format!("{}/time_entries/{}", self.base_url, time_entry.id);
+        return self
+            .put::<TimeEntry, TimeEntry>(url, &stopped_time_entry)
+            .await;
+    }
+}
+
+impl V9ApiClient {
     pub fn from_credentials(
         credentials: credentials::Credentials,
-    ) -> ResultWithDefaultError<ApiClient> {
+    ) -> ResultWithDefaultError<V9ApiClient> {
         let auth_string = credentials.api_token + ":api_token";
         let header_content = "Basic ".to_string() + base64::encode(auth_string).as_str();
         let mut headers = header::HeaderMap::new();
@@ -28,52 +81,6 @@ impl ApiClient {
             base_url: "https://track.toggl.com/api/v9".to_string(),
         };
         return Ok(api_client);
-    }
-
-    pub async fn get_user(&self) -> ResultWithDefaultError<User> {
-        let url = format!("{}/me", self.base_url);
-        return self.get::<User>(url).await;
-    }
-
-    pub async fn get_running_time_entry(&self) -> ResultWithDefaultError<Option<TimeEntry>> {
-        let url = format!("{}/me/time_entries/current", self.base_url);
-        return self.get::<Option<TimeEntry>>(url).await;
-    }
-
-    pub async fn get_time_entries(&self) -> ResultWithDefaultError<Vec<TimeEntry>> {
-        let url = format!("{}/me/time_entries", self.base_url);
-        return self.get::<Vec<TimeEntry>>(url).await;
-    }
-
-    pub async fn start_time_entry(
-        &self,
-        time_entry: TimeEntry,
-    ) -> ResultWithDefaultError<TimeEntry> {
-        let url = format!("{}/time_entries", self.base_url);
-        let time_entry_to_create = TimeEntry {
-            created_with: Some(CLIENT_NAME.to_string()),
-            ..time_entry
-        };
-        return self
-            .post::<TimeEntry, TimeEntry>(url, &time_entry_to_create)
-            .await;
-    }
-
-    pub async fn stop_time_entry(
-        &self,
-        time_entry: TimeEntry,
-    ) -> ResultWithDefaultError<TimeEntry> {
-        let stop_time = Utc::now();
-        let stopped_time_entry = TimeEntry {
-            stop: Some(stop_time),
-            duration: (stop_time - time_entry.start).num_seconds(),
-            ..time_entry
-        };
-
-        let url = format!("{}/time_entries/{}", self.base_url, time_entry.id);
-        return self
-            .put::<TimeEntry, TimeEntry>(url, &stopped_time_entry)
-            .await;
     }
 
     async fn get<T: de::DeserializeOwned>(&self, url: String) -> ResultWithDefaultError<T> {
