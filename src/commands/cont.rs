a35554e102c86debcd793e616fdd0c614dc34c99
Shantanu Raj <s@sraj.me>
2021-07-01 23:29:47 +0200

    🪟  Fix windows build
    Co-authored-by: William Barbosa <heytherewill@gmail.com>

--- src/commands/cont.rs
+++ src/commands/cont.rs
@@ -2,28 +2,10 @@ use crate::api;
 use crate::models;
 use api::ApiClient;
 use chrono::Utc;
-use colored::control;
 use colored::Colorize;
 use models::{ResultWithDefaultError, TimeEntry};
-use skim::prelude::*;
 
 pub struct ContinueCommand;
-
-impl SkimItem for TimeEntry {
-    fn text(&self) -> Cow<str> {
-        // Wrap text calculation in this block
-        // to disable outputing the color keycodes
-        // since the filter window doesn't support them
-        control::set_override(false);
-        let self_as_plainstring = Cow::from(self.to_string());
-        control::unset_override();
-        self_as_plainstring
-    }
-
-    fn output(&self) -> Cow<str> {
-        Cow::from(self.id.to_string())
-    }
-}
 
 impl ContinueCommand {
     pub async fn execute(
@@ -74,7 +56,7 @@ fn get_time_entry_to_continue(
     interactive: bool,
 ) -> Option<TimeEntry> {
     if interactive {
-        get_time_entry_from_user(time_entries)
+        autocomplete::get_time_entry_from_user(time_entries)
     } else {
         get_first_stopped_time_entry(time_entries, running_time_entry)
     }
@@ -92,48 +74,83 @@ fn get_first_stopped_time_entry(
     return time_entries.get(continue_entry_index).cloned();
 }
 
-fn get_time_entry_from_user(time_entries: Vec<TimeEntry>) -> Option<TimeEntry> {
-    let (options, source) = get_skim_configuration(time_entries.clone());
-    Skim::run_with(&options, Some(source))
-        .map(|output| {
-            if output.is_abort {
-                println!("{}", "Operation cancelled".red());
-                Vec::new()
-            } else {
-                output.selected_items
-            }
-        })
-        .map(|selected_items| {
-            selected_items
-                .first()
-                .map(|item| item.output().parse::<i64>().unwrap())
-        })
-        .and_then(|selected_time_entry_id| match selected_time_entry_id {
-            Some(time_entry_id) => time_entries
-                .iter()
-                .find(|time_entry| time_entry.id == time_entry_id)
-                .cloned(),
-            _ => None,
-        })
+#[cfg(not(unix))]
+mod autocomplete {
+    use super::*;
+
+    pub fn get_time_entry_from_user(_time_entries: Vec<TimeEntry>) -> Option<TimeEntry> {
+        panic!(
+            "{}",
+            "Interactive continue is currently not supported on Windows".red()
+        )
+    }
 }
 
-fn get_skim_configuration(
-    time_entries: Vec<TimeEntry>,
-) -> (SkimOptions<'static>, SkimItemReceiver) {
-    let options = SkimOptionsBuilder::default()
-        // Set viewport to take entire screen
-        .height(Some("100%"))
-        // Disable multiselect
-        .multi(false)
-        .build()
-        .unwrap();
+#[cfg(unix)]
+mod autocomplete {
+    use super::*;
+    use colored::control;
+    use skim::prelude::*;
 
-    let (sender, source): (SkimItemSender, SkimItemReceiver) = unbounded();
-    for time_entry in time_entries {
-        // Send time-entries to Skim receiver
-        let _ = sender.send(Arc::new(time_entry.clone()));
+    impl SkimItem for TimeEntry {
+        fn text(&self) -> Cow<str> {
+            // Wrap text calculation in this block
+            // to disable outputing the color keycodes
+            // since the filter window doesn't support them
+            control::set_override(false);
+            let self_as_plainstring = Cow::from(self.to_string());
+            control::unset_override();
+            self_as_plainstring
+        }
+
+        fn output(&self) -> Cow<str> {
+            Cow::from(self.id.to_string())
+        }
     }
-    // Complete sender transaction to signal no new items will be added after this
-    drop(sender);
-    (options, source)
+
+    pub fn get_time_entry_from_user(time_entries: Vec<TimeEntry>) -> Option<TimeEntry> {
+        let (options, source) = get_skim_configuration(time_entries.clone());
+        Skim::run_with(&options, Some(source))
+            .map(|output| {
+                if output.is_abort {
+                    println!("{}", "Operation cancelled".red());
+                    Vec::new()
+                } else {
+                    output.selected_items
+                }
+            })
+            .map(|selected_items| {
+                selected_items
+                    .first()
+                    .map(|item| item.output().parse::<i64>().unwrap())
+            })
+            .and_then(|selected_time_entry_id| match selected_time_entry_id {
+                Some(time_entry_id) => time_entries
+                    .iter()
+                    .find(|time_entry| time_entry.id == time_entry_id)
+                    .cloned(),
+                _ => None,
+            })
+    }
+
+    fn get_skim_configuration(
+        time_entries: Vec<TimeEntry>,
+    ) -> (SkimOptions<'static>, SkimItemReceiver) {
+        let options = SkimOptionsBuilder::default()
+            // Set viewport to take entire screen
+            .height(Some("100%"))
+            // Disable multiselect
+            .multi(false)
+            .build()
+            .unwrap();
+
+        let (sender, source): (SkimItemSender, SkimItemReceiver) = unbounded();
+        for time_entry in time_entries {
+            // Send time-entries to Skim receiver
+            let _ = sender.send(Arc::new(time_entry.clone()));
+        }
+        // Complete sender transaction to signal no new items will be added after this
+        drop(sender);
+        (options, source)
+    }
 }
