fbbb8f02c5998190a072a1c49546bc86ac6dcd13
William Barbosa <heytherewill@gmail.com>
2022-04-18 08:05:19 +0200

    📚 Document changes

--- src/arguments.rs
+++ src/arguments.rs
@@ -21,8 +21,13 @@ pub enum Command {
     Auth {
         api_token: String,
     },
+    #[structopt(
+        about = "Start a new time entry. Call with no arguments to start in interactive mode."
+    )]
     Start {
-        #[structopt(short, long)]
+        #[structopt(
+            about = "Description of the time entry. If not specified, the user will be prompted to enter it."
+        )]
         description: Option<String>,
         #[structopt(short, long)]
         project: Option<String>,
