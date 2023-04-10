268ece4315066b3ed0d56084e355abf07fa5a541
Shantanu Raj <s@sraj.me>
2023-04-10 16:21:15 +0200

    📐 Refactor utilities

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,7 +1,38 @@
-use std::path::Path;
+use std::{
+    io::{self, Write},
+    path::Path,
+};
+
+use colored::Colorize;
 
 pub fn remove_trailing_newline(value: String) -> String {
     value.trim_end().to_string()
+}
+
+pub fn read_from_stdin(text: &str) -> String {
+    print_without_buffer(text);
+    let mut result = String::new();
+    io::stdin()
+        .read_line(&mut result)
+        .expect("Failed to read line");
+    remove_trailing_newline(result)
+}
+
+pub fn read_from_stdin_with_constraints(text: &str, valid_values: &[String]) -> String {
+    loop {
+        let result = read_from_stdin(text);
+        if valid_values.contains(&result) {
+            return result;
+        } else {
+            let error_message = format!(
+                "Invalid value \"{}\". Valid values are: {}\n",
+                result,
+                valid_values.join(", ")
+            )
+            .red();
+            print_without_buffer(&error_message);
+        }
+    }
 }
 
 pub fn open_path_in_editor(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
@@ -12,4 +43,9 @@ pub fn open_path_in_editor(path: &Path) -> Result<(), Box<dyn std::error::Error>
         .expect("Failed to spawn editor");
     child.wait().expect("Failed to wait for editor");
     Ok(())
+}
+
+fn print_without_buffer(text: &str) {
+    print!("{}", text);
+    io::stdout().flush().unwrap();
 }
