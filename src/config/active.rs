376ae9d51709f7612eb576c38b3bfab4ee02896e
Shantanu Raj <s@sraj.me>
2023-09-04 20:41:59 +0200

    🌟 Integrate configuration into tracking (#62)
    - 📦 Add `get_active_config` to TrackConfig
    
    - 🌟 Prefill time-entry using active config
    
    Closes #45

--- src/config/active.rs
+++ src/config/active.rs
@@ -6,8 +6,7 @@ impl ConfigActiveCommand {
     pub async fn execute() -> ResultWithDefaultError<()> {
         let config_path = super::locate::locate_config_path()?;
         let track_config = super::parser::get_config_from_file(config_path)?;
-        let current_dir = std::env::current_dir()?;
-        println!("{}", track_config.get_branch_config_for_dir(&current_dir));
+        println!("{}", track_config.get_active_config()?);
         Ok(())
     }
 }
