fc74979b7e4af1417b27cd9f2919471a1fb016b4
Shantanu Raj <s@sraj.me>
2024-09-22 18:36:05 +0200

    🪵 Add expectation  errors

--- src/parcel.rs
+++ src/parcel.rs
@@ -15,20 +15,20 @@ pub trait Parcel {
     {
         let contents = self.serialize();
 
-        let dir = tempdir().map_err(|e| e.to_string())?;
+        let dir = tempdir().expect("Failed to create temp directory");
         let file_path = dir.path().join("toggl.txt");
 
-        let mut file = File::create_new(file_path.clone()).map_err(|e| e.to_string())?;
+        let mut file = File::create_new(file_path.clone()).expect("Failed to create file");
         file.write_all(contents.as_bytes())
             .expect("Failed to write current time-entry to file");
 
-        utilities::open_path_in_editor(&file_path).map_err(|e| e.to_string())?;
+        utilities::open_path_in_editor(&file_path).expect("Failed to open file in editor");
         drop(file);
 
         let contents = fs::read_to_string(file_path)
-            .map_err(|e| e.to_string())?;
+            .expect("Failed to read file time-entry editing in editor");
 
-        dir.close().map_err(|e| e.to_string())?;
+        dir.close().expect("Failed to clear temp directory");
 
         Ok(self.deserialize(&contents))
     }
