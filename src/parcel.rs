5c7e0d9d84eaf37a2e9434e7f4ad1d7738d34031
Shantanu Raj <s@sraj.me>
2024-09-22 18:33:53 +0200

    🔨 Fix incorrect file contents being read
    - Reusing the same handle wasn't helping

--- src/parcel.rs
+++ src/parcel.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
-use std::io::{Read, Seek, Write};
+use std::fs::{self, File};
+use std::io::Write;
 
 use tempfile::tempdir;
 
@@ -22,14 +22,12 @@ pub trait Parcel {
         file.write_all(contents.as_bytes())
             .expect("Failed to write current time-entry to file");
 
-        utilities::open_path_in_editor(file_path).map_err(|e| e.to_string())?;
-        file.rewind().map_err(|e| e.to_string())?;
+        utilities::open_path_in_editor(&file_path).map_err(|e| e.to_string())?;
+        drop(file);
 
-        let mut contents = String::new();
-        file.read_to_string(&mut contents)
+        let contents = fs::read_to_string(file_path)
             .map_err(|e| e.to_string())?;
 
-        drop(file);
         dir.close().map_err(|e| e.to_string())?;
 
         Ok(self.deserialize(&contents))
